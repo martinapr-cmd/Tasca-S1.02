9dedc894354d3df9058acf66e1800537a53e14ed
Mar <marpouradressa@gmail.com>
2026-02-10 12:58:59 +0100

    integer, double, string and boolean practice.

---
+++ level-01/exercise-01.php
@@ -0,0 +1,29 @@
+<?php
+
+/*  Defineix una variable de cada tipus: integer, double, string i boolean. 
+  Imprimeix-les per pantalla/terminal.
+
+ Després crea una constant que inclogui el teu nom i mostra-ho en format títol
+  per pantalla o imprimeix-la per terminal. */
+
+$integer = 5;
+echo "$integer\n";
+var_dump($integer);
+
+$double = 5.3;
+echo "$double\n";
+var_dump($double);
+
+$string = "string";
+echo "$string\n";
+var_dump($string);
+echo "this is a " . $string . "!\n";
+
+$boolean = true;
+echo "$boolean\n"; 
+var_dump($boolean);
+
+
+
+
+ ?>
