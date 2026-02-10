e2fe173946ada701fa24a49e091be13afb37a809
Mar <marpouradressa@gmail.com>
2026-02-10 18:57:19 +0100

    Exercise 6. Function, if/else, rand().

---
+++ level-01/exercise-06.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Charlie em va mossegar el dit! Charlie et mossegarà el dit exactament el 50% del temps.
+Escriu La funció isBitten() que retorna TRUE amb un 50% de probabilitat i FALSE en cas contrari.
+
+Consell: pot ser que la funció rand () et resulti útil.
+*/
+
+echo "Charlie bit my finger! But was it true?\n";
+
+function isBitten() {
+    $randomNumber = rand(0, 1); // generates either 0 or 1. 
+    return $randomNumber === 1; // === will tell if randomNumber exactly equals to 1! 
+}
+
+if (isBitten()) {
+    echo "It was true :-(\n";
+} else {
+    echo "It was false :-D\n";
+}
+
+
+
+
+
+?>
